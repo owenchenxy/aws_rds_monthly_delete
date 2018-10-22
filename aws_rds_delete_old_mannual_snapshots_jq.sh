d5f1c9f4734038594bbcecd63fdb66391773ead3
owen chen <xianyao.chen@autodesk.com>
2018-10-22 17:23:39 +0800

    1

--- aws_rds_delete_old_mannual_snapshots_jq.sh
+++ aws_rds_delete_old_mannual_snapshots_jq.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-aws rds describe-db-snapshots --profile stg > db_jqlist
-numberOfSnapshots=$(cat db_jqlist | jq ".DBSnapshots | length")
+ENV_FILE=/Users/chenxi/BIM360/env_shell/dm/stg.sh
+source $ENV_FILE
+db_jqlist=$(aws rds describe-db-snapshots --profile stg)
+if [[ $? != 0 ]];then exit 1;fi
+numberOfSnapshots=$(echo $db_jqlist | jq ".DBSnapshots | length")
 for i in $(seq 0 $(($numberOfSnapshots-1)))
 do
-    SnapshotType=$(cat db_jqlist | jq .DBSnapshots[$i].SnapshotType)
+    SnapshotType=$(echo $db_jqlist | jq .DBSnapshots[$i].SnapshotType)
     if [[ $SnapshotType == "\"manual\"" ]]
     then
-        DBSnapshotIdentifier=$(cat db_jqlist | jq .DBSnapshots[$i].DBSnapshotIdentifier)
-        SnapshotCreateTime=$(cat db_jqlist | jq .DBSnapshots[$i].SnapshotCreateTime)
+        DBSnapshotIdentifier=$(echo $db_jqlist | jq .DBSnapshots[$i].DBSnapshotIdentifier)
+        SnapshotCreateTime=$(echo $db_jqlist | jq .DBSnapshots[$i].SnapshotCreateTime)
         SnapshotCreateTime=$(date -d $SnapshotCreateTime +%s)
         oneMonthAgo=$(date -d '1 month ago' +%s)
         if [[ $SnapshotCreateTime -lt $oneMonthAgo ]]
         then
-            aws rds delete-db-snapshot --db-snapshot-identifier $DBSnapshotIdentifier --profile stg
+            aws rds describe-db-snapshot --db-snapshot-identifier=$DBSnapshotIdentifier --profile stg
+            #aws rds delete-db-snapshot --db-snapshot-identifier $DBSnapshotIdentifier --profile stg
         fi
     fi
 done
