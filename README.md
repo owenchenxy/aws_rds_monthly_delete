e01159d95d2f0f6b459fe18c5d14a18666bbdcf6
owen chen <xianyao.chen@autodesk.com>
2018-10-22 17:24:47 +0800

    2

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # aws_rds_monthly_delete
+##This script will delete the aws rds snapshots matches the blow conditions:
+##  1) SnapshotType is manual
+##  2) Create at more that 1 month ago
+##Prequists for this scripts:
+##  1) AWScli is installed
+##  2) AWS credential is correctly configured.
--- aws_rds_delete_old_mannual_snapshots.sh
+++ aws_rds_delete_old_mannual_snapshots.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-##This script will delete the aws rds snapshots matches the blow conditions:
-##  1) SnapshotType is manual
-##  2) Create at more that 1 month ago
-##Prequists for this scripts:
-##  1) AWScli is installed
-##  2) AWS credential is correctly configured.
+
 aws rds describe-db-snapshots --profile stg > db_list
 while read LINE
 do
